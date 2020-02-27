fd5176a35d0bf972d2b17676d02b48d970dd94c6
tyh <756467256@qq.com>
2020-02-27 12:01:39 +0800

    {YunHan}{feat}--{JAVA-Caption3-work4}:IO

--- io/src/main/java/com/thoughtworks/io/FileUtil.java
+++ io/src/main/java/com/thoughtworks/io/FileUtil.java
@@ -1,7 +1,6 @@
 package com.thoughtworks.io;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 
 public class FileUtil {
 
@@ -12,7 +11,32 @@ public class FileUtil {
      * <p>
      * 例如把a文件夹(a文件夹下有1.txt和一个空文件夹c)复制到b文件夹，复制完成以后b文件夹下也有一个1.txt和空文件夹c
      */
-    public static void copyDirectory(File from, File to) throws IOException {
 
+    public static void main(String[] args) throws IOException {
+        File from = new File(".\\io\\src\\main\\from");
+        File to = new File(".\\io\\src\\main\\to");
+        copyDirectory(from, to);
+    }
+
+    public static void copyDirectory(File from, File to) throws IOException {
+        if (!to.exists()) {
+            to.mkdirs();
+        }
+        File[] files = from.listFiles();
+        for (File fileName : files) {
+            if (fileName.isDirectory()) {
+                copyDirectory(fileName, new File(to, fileName.getName()));
+            } else {
+                File copyFile = new File(to, fileName.getName());
+                copyFile.createNewFile();
+                FileOutputStream fos = new FileOutputStream(copyFile);
+                FileInputStream fis = new FileInputStream(fileName);
+                int len = 0;
+                while ((len = fis.read()) != -1) {
+                    fos.write(len);
+                }
+            }
+        }
     }
 }
+
